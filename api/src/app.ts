adf6dba8274e471c4d1a3529e31d0944d1b6d40a
edwinwalela <edwinwalela@gmail.com>
2022-11-19 14:25:04 +0300

    setup express server

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,9 @@
-import { Express } from "express";
-import express from "express";
+import { Express } from 'express';
+import express from 'express';
+import config from './config/config';
 
-const app:Express = express();
+const app: Express = express();
+
+app.listen(config.httpPort, () => {
+	console.log(`Listening for requests on port ${config.httpPort}`);
+});
