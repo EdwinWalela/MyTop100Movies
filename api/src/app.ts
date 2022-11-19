856a7f46266b1b5eb29435008d787c2d609ac847
edwinwalela <edwinwalela@gmail.com>
2022-11-19 14:56:31 +0300

    connect to db

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,20 @@
 import { Express } from 'express';
 import express from 'express';
 import config from './config/config';
+import { exit } from 'process';
+import db from './config/db';
 
 const app: Express = express();
+
+(async () => {
+	try {
+		await db.connect();
+		console.log('Connected to database');
+	} catch (error) {
+		console.log( 'failed to connect to database', error);
+		exit(1);
+	}
+})();
 
 app.listen(config.httpPort, () => {
 	console.log(`Listening for requests on port ${config.httpPort}`);
