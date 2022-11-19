cb0add5d7d452d62fd969281372ee1b11442691f
edwinwalela <edwinwalela@gmail.com>
2022-11-19 15:47:17 +0300

    login route

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -15,17 +15,40 @@ const registerUser = async (req: Request, res: Response, next: NextFunction) => 
 
   try {
     id = await service.createUser(user);
-    res.status(201).send({
+    return res.status(201).send({
       message: 'User created',
       id:id
     })
   } catch (error:any) {
-    res.status(400).send({
-      error
+    return res.status(400).send({
+      error:error.message
     })
   }
 }
 
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  const {email,password} = req.body
+
+  if(!email || !password){
+    return res.status(400).send({
+      message:'Required fields missing'
+    })
+  }
+
+  try {
+    const token = await service.login(email,password);
+    return res.status(200).send({
+      token
+    })
+  }catch(error:any){
+    return res.status(400).send({
+      error:error.message
+    })
+  }
+
+}
+
 export default{
-  registerUser
+  registerUser,
+  login
 }
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -4,5 +4,6 @@ import controller from "../controllers/auth"
 const router = express.Router()
 
 router.post('/register', controller.registerUser)
+router.post('/login', controller.login)
 
 export default router
