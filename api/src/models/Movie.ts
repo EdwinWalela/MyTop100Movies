276cef38b319b2e384faef44e309af706772b7ea
edwinwalela <edwinwalela@gmail.com>
2022-11-20 12:52:38 +0300

    movie db api provider

---
+++ api/src/models/Movie.ts
@@ -0,0 +1,13 @@
+class Movie {
+	id: number;
+	title: string;
+	releaseDate: string;
+
+	constructor(id: number, title: string, releaseDate: string) {
+		this.id = id;
+		this.title = title;
+		this.releaseDate = releaseDate;
+	}
+}
+
+export default Movie;
